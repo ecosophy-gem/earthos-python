59eb6843ae36d096ef6ef27fafa569b48bda01ea
Smári McCarthy <smari@ecosophy.is>
2024-07-01 18:14:33 +0000

    Update setup metadata

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='Ecosophy EarthOS API bindings',
+    long_description=open('README.md').read(),
     author='Ecosophy',
-    license='BSD',
+    author_email='info@ecosophy.is',
+    license='Apache',
+    url='https://ecosophy.is/',
     install_requires=[
         'requests',
         'Pillow',
@@ -14,5 +17,16 @@ setup(
     ],
     tests_require=[
         'pytest',
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "Topic :: Scientific/Engineering :: Oceanography",
     ],
 )
